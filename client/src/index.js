5370a6b160ffdd3ccfc4b3714d52aac42475da3f
abdousmounir <mounirabdous@gmail.com>
2018-12-12 19:48:54 -0500

    isomorphic added

--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DashApp from './dashApp';
+import registerServiceWorker from './registerServiceWorker';
 
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from "./store/reducers/order";
-import authReducer from "./store/reducers/auth";
-import * as watch from "./store/sagas/index";
+ReactDOM.render(<DashApp />, document.getElementById('root'));
 
-const composeEnhancers = compose;
-// process.env.NODE_ENV === "development"
-//   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//   : compose;
-
-const rootReducer = combineReducers({
-  burgerBuilder: burgerBuilderReducer,
-  order: orderReducer,
-  auth: authReducer
-});
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
-);
-
-sagaMiddleware.run(watch.watchAuth);
-sagaMiddleware.run(watch.watchOrder);
-sagaMiddleware.run(watch.watchBurgerBuilder);
-
-const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
-
-ReactDOM.render(app, document.getElementById("root"));
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./dashApp.js', () => {
+    const NextApp = require('./dashApp').default;
+    ReactDOM.render(<NextApp />, document.getElementById('root'));
+  });
+}
 registerServiceWorker();
